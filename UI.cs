39cf389f48f2d5103ca670b028ad9022149b30a5
ndhungw <56079549+ndhungw@users.noreply.github.com>
2019-11-30 23:46:41 +0700

    Bắt lỗi System.ArgumentNullException

--- UI.cs
+++ UI.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,17 @@ namespace WpfApp_Windows_Project2
     {
         public static void setLeftTopImage(Image _selectedBitmap, double left, double top)
         {
-            Canvas.SetLeft(_selectedBitmap, left);
-            Canvas.SetTop(_selectedBitmap, top);
+            try
+            {
+                Canvas.SetLeft(_selectedBitmap, left);
+                Canvas.SetTop(_selectedBitmap, top);
+            }
+            catch (System.ArgumentNullException e)
+            {
+                Debug.WriteLine($"Error: {e.Message} \nĐây là trường hợp người dùng click quá nhanh" +
+                    $" nên sau khi FileDialog vừa tắt thì chuột click vào cropImage ở phần giao diện" +
+                    $" trong khi hình chưa được cắt xong");
+            }
         }
 
     }
